fc87c116963aac43c165976fe6f686ae81ee5a12
zardoru <forsakengmex@gmail.com>
2016-06-03 08:43:49 -0400

    Generate some javascript.

--- src/Smaug/CodeGen.hs
+++ src/Smaug/CodeGen.hs
@@ -11,11 +11,43 @@ import Smaug.LangADT
 generateCode' :: [BodyExpr] -> String
 generateCode' = concat . map generateCode 
 
+infixGenerate op l r =
+    generateCodeEx l ++ op ++ generateCodeEx r
+    
+generateCodeEx :: Expr -> String
+generateCodeEx (Add l r) = infixGenerate "+" l r
+generateCodeEx (Mul l r) = infixGenerate "*" l r
+generateCodeEx (Sub l r) = infixGenerate "-" l r
+generateCodeEx (Div l r) = infixGenerate "/" l r
+generateCodeEx (Lt l r) = infixGenerate "<" l r
+generateCodeEx (Gt l r) = infixGenerate ">" l r
+generateCodeEx (Leq l r) = infixGenerate "<=" l r
+generateCodeEx (Geq l r) = infixGenerate ">=" l r
+generateCodeEx (Neq l r) = infixGenerate "!=" l r
+generateCodeEx (Eq l r) = infixGenerate "===" l r
+generateCodeEx (And l r) = infixGenerate "&&" l r
+generateCodeEx (Or l r) = infixGenerate "||" l r
+generateCodeEx (Ident s) = s
+generateCodeEx (Lit d) = show d
+generateCodeEx (QuoteStr s) = "\"" ++ s ++ "\""
+generateCodeEx (FunCallExpr s x) = s ++ 
+                    "(" ++ (intercalate "," $ map generateCodeEx x) ++ ")"
+generateCodeEx x = error $ "Unimplemented expr codegen path " ++ show x
+
 generateCode :: BodyExpr -> String
 generateCode expr = 
     case expr of
         (MainBody lxpr) -> generateCode' lxpr
-        _ -> error "Unimplemented codegen path."
+        (LetExpr i xpr) -> "var " ++ i ++ "=" ++ generateCodeEx xpr ++ ";"
+        (AssnExpr i xpr) -> i ++ "=" ++ generateCodeEx xpr ++ ";"
+        (WhileExpr xpr bxpr) -> "while(" ++ 
+                                generateCodeEx xpr ++ "){" ++ 
+                                generateCode' bxpr ++ "}"
+        (IfExpr xpr bxpr) -> "if(" ++
+                              generateCodeEx xpr ++ "){" ++
+                              generateCode' bxpr ++ "}"
+        (BodyCallExpr xpr) -> generateCodeEx xpr ++ ";"
+        _ -> error $ "Unimplemented codegen path " ++ show expr
 
 smaugGenerateCode :: FilePath -> BodyExpr -> IO ()
 smaugGenerateCode p expr = do
